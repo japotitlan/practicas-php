32c38eb08f75d7732624f2ac3e4d8789a942da47
Fernando Reyes <hapoweiser@gmail.com>
2022-12-10 11:10:01 -0700

    die function

---
+++ 4_dieFunction/index.php
@@ -0,0 +1,14 @@
+<?php 
+    // La funcion de die() funciona similar a un break en un ciclo, detiene el proceso.
+    
+    echo "Mensaje 1"."<br>";
+    echo "Mensaje 2"."<br>";
+    echo "Mensaje 3"."<br>";
+
+    die("cagaste");
+
+    echo "Mensaje 4"."<br>";
+    echo "Mensaje 5"."<br>";
+    echo "Mensaje 6"."<br>";
+
+?>
