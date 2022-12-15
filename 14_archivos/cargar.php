1f1eb7daf9a6a1fb571d2b7ca90d8cec6142ec93
Fernando Reyes <hapoweiser@gmail.com>
2022-12-14 17:07:42 -0700

    simon calando

--- 14_archivos/cargar.php
+++ 14_archivos/cargar.php
@@ -5,9 +5,14 @@
     echo '</pre>';
 
     $archivo_ext = PATHINFO($archivo['name'], PATHINFO_EXTENSION);
+    $nuevo_nombre = uniqid("kakino-").'.'.$archivo_ext;
 
     echo '<br>';
     echo 'Nombre del archivo   : '.$archivo['name'].'<br>';
-    echo 'Tamaño del archivo   : '.$archivo['size'].'KBs'.' | '.($archivo['size']/1024).'MBs'.'<br>';
+    echo 'Ubicacion temporal   : '.$archivo['tmp_name'].'<br>';
+    echo 'Tamaño del archivo   : '.$archivo['size'].' Bytes'.' | '.($archivo['size']/1024).' KiloBytes'.'<br>';
     echo 'Extension del archivo: '.$archivo_ext.'<br>';
+    echo 'Nombre custom: '.$nuevo_nombre;
+
+    echo '<br>'.'<br>'.'Tamaño del archivo   : '.$archivo['size'];
 ?>
