10ac54a0b622537c00193600a5269077225edd66
Fernando Reyes <hapoweiser@gmail.com>
2022-12-16 10:05:46 -0700

    yatusabe papapap

--- 14_archivos/cargar.php
+++ 14_archivos/cargar.php
@@ -1,18 +1,30 @@
 <?php 
-    $archivo = $_FILES['archivo'];
-    echo '<pre>';
-    print_r($archivo);
-    echo '</pre>';
+    // Verificar si se envio archivo
+    // error 4 significa que no se envio ningun archivo
+    if($_FILES['archivo']['error'] != 4){
 
-    $archivo_ext = PATHINFO($archivo['name'], PATHINFO_EXTENSION);
-    $nuevo_nombre = uniqid("kakino-").'.'.$archivo_ext;
+        $archivo = $_FILES['archivo'];
 
-    echo '<br>';
-    echo 'Nombre del archivo   : '.$archivo['name'].'<br>';
-    echo 'Ubicacion temporal   : '.$archivo['tmp_name'].'<br>';
-    echo 'Tamaño del archivo   : '.$archivo['size'].' Bytes'.' | '.($archivo['size']/1024).' KiloBytes'.'<br>';
-    echo 'Extension del archivo: '.$archivo_ext.'<br>';
-    echo 'Nombre custom: '.$nuevo_nombre;
+        echo '<pre>';
+        print_r($archivo);
+        echo '</pre>';
 
-    echo '<br>'.'<br>'.'Tamaño del archivo   : '.$archivo['size'];
+        $archivo_ext = PATHINFO($archivo['name'], PATHINFO_EXTENSION);
+        $nuevo_nombre = uniqid("kakino-").'.'.$archivo_ext;
+
+        echo '<br>';
+        echo 'Nombre del archivo   : '.$archivo['name'].'<br>';
+        echo 'Ubicacion temporal   : '.$archivo['tmp_name'].'<br>';
+        echo 'Tamaño del archivo   : '.$archivo['size'].' Bytes'.' | '.($archivo['size']/1024).' KiloBytes'.'<br>';
+        echo 'Extension del archivo: '.$archivo_ext.'<br>';
+        echo 'Nombre custom: '.$nuevo_nombre;
+
+        echo '<br>'.'<br>'.'Tamaño del archivo   : '.$archivo['size'];
+
+    } else{
+
+        echo 'No se seleccionó ningun archivo.';
+
+    }
+    
 ?>
